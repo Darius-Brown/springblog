84923bf3e4224a6e8caf6f02e1dea5efdab88f0c
Darius Brown <Brown.darius.r@gmail.com>
2021-06-09 10:47:24 -0500

    controller

---
+++ src/main/java/com/codeup/springblog/Controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.codeup.springblog.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+}
