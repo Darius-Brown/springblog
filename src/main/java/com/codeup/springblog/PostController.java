b1a2b95b2474014ed3d2ecf0eb69478bde8d319e
Darius Brown <Brown.darius.r@gmail.com>
2021-06-14 10:15:02 -0500

    feat:

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,4 +1,15 @@
 package com.codeup.springblog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PostController {
+
+    @GetMapping("/post")
+    public String index(Model model){
+        model.addAttribute("posts", "posts index page");
+        return "index";
+    }
 }
